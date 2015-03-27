858225d5e05f081eb64ceea085fc5471c4f1b3f3
johanbjerre <jubjerre@yahoo.se>
2015-03-27 17:16:49 +0100

    added some loaders stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,26 +1,27 @@
-(function() {
+(function () {
 
-	'use strict';
+    'use strict';
 
-	/**
-	 * @ngdoc function
-	 * @name shSpringMeetingAngularApp.controller:MainCtrl
-	 * @description
-	 * # MainCtrl
-	 * Controller of the shSpringMeetingAngularApp
-	 */
-	angular.module('shSpringMeetingAngularApp')
-	  .controller('MainCtrl', mainCtrl);
+    /**
+     * @ngdoc function
+     * @name shSpringMeetingAngularApp.controller:MainCtrl
+     * @description
+     * # MainCtrl
+     * Controller of the shSpringMeetingAngularApp
+     */
+    angular.module('shSpringMeetingAngularApp')
+            .controller('MainCtrl', mainCtrl);
 
-	function mainCtrl($scope, $log, $http) {
-		var vm = this;
-	    vm.me = {};
-	    vm.save = save;
+    function mainCtrl($scope, $log, $http) {
+        var vm = this;
+        vm.me = {};
+        vm.save = save;
 
-		$http.get('/employees/me').then(function(response) {
-			vm.me = response.data;
-		});
+        $http.get('/employees/me').then(function (response) {
+            vm.me = response.data;
+        });
 
+<<<<<<< HEAD
 		function save() {
 			vm.saving = true;
 			$http.put('/employees/me', vm.me).then(function(response) {
@@ -31,5 +32,17 @@
 			});
 		}
 	 }
+=======
+        function save(me) {
+            vm.saving = true;
+            $http.put('/employees/me', me).then(function (response) {
+                vm.saving = false;
+            }, function (errorResponse) {
+                $log.error(errorResponse);
+                vm.saving = false;
+            });
+        }
+    }
+>>>>>>> added some loaders stuff
 
 })();
